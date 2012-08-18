b78396a3f9019ee65311bb59547491eb1d5f2472
Philip Horger <campadrenalin@gmail.com>
2012-08-17 17:19:02 -0700

    Proper handling of tempfiles. No more mysterious 't'

--- dbcps/backends/adbm.py
+++ dbcps/backends/adbm.py
@@ -1,5 +1,6 @@
 from dbcps.dbh import DBH
 import anydbm
+import os
 
 print anydbm
 
@@ -8,8 +9,8 @@ class ADBM(DBH):
     Uses the anydbm module to store data to disc.
 
     >>> from dbcps import storage
-    >>> from tempfile import mktemp
-    >>> fname = mktemp()[1]
+    >>> from tempfile import mkstemp
+    >>> fname = mkstemp()[1]
     >>> backend = ('adbm', fname, ['rotate', -91])
     >>> s = storage.Storage([backend])
     >>> s['hello'] = 'world'
@@ -31,6 +32,9 @@ class ADBM(DBH):
 
     def __init__(self, path, encryptor, handle='anydbm', filemode='c'):
         DBH.__init__(self, handle, encryptor)
+        filesize = os.stat(path).st_size
+        if filesize == 0:
+            os.remove(path)
         self.db = anydbm.open(path, filemode)
 
     def get(self, k):
