7ee431a8b1f1a8bfefde239615b9bcbabd493756
Philip Horger <campadrenalin@gmail.com>
2013-03-12 20:02:45 -0700

    #7 Finally got tox tests to pass locally, setting up Travis now

--- dbcps/sinks/adbm.py
+++ dbcps/sinks/adbm.py
@@ -35,3 +35,11 @@ class ADBM(Sink):
     def __init__(self, path, origin = None, filemode='c'):
         Sink.__init__(self, origin)
         self.backend = anydbm.open(path, filemode)
+
+    def __contains__(self, k):
+        if (anydbm.__name__ == 'anydbm'):
+            return k in self.backend
+        elif (anydbm.__name__ == 'dbm'):
+            return k.encode('utf-8') in self.backend
+        else:
+            raise NotImplementedError("anydbm wasn't imported via anydbm or dbm, not sure what's going on!")
